19cdc22a7bf273248d36382d5d350a2a9b22acf1
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-04 13:53:55 +0300

    Making a copy of the state in ProfileReducer

--- src/index.tsx
+++ src/index.tsx
@@ -5,24 +5,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { RootStateType } from './Redux/store';
 import { Provider } from 'react-redux';
 
-export const rerenderEntireTree = (state: RootStateType) => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-};
-
-rerenderEntireTree(store.getState());
-store.subscribe(() => {
-  let state = store.getState();
-  rerenderEntireTree(state);
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
