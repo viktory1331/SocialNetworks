6cbafbe7ed06c108fba34e0378078cc08421c04b
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-17 22:44:11 +0300

    I passed the props and prescribed the styles

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { InitialStateType } from '../../Redux/auth-reducer';
 import s from './Header.module.css';
+import { UserType } from '../../Redux/users-reducer';
 
-export const Header = () => {
+type HeaderPropsType = {
+  isAuth: boolean,
+  login: null | UserType
+  setAuthUserData: (
+    login: null | UserType,
+    id: null | UserType,
+    email: null | UserType
+  ) => void;
+};
+
+export const Header = (props: HeaderPropsType) => {
   return (
     <header className={s.header}>
       <img src="https://www.freepnglogos.com/uploads/company-logo-png/company-logo-transparent-png-19.png" />
+
+      <div className={s.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+      </div>
     </header>
   );
 };
