cd30f7a5fd528ceae7f4dd8fe9dff5ecd20fc3ac
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-18 20:42:50 +0300

    Added the usersAPI

--- src/Components/Users/UsersContainer.tsx
+++ src/Components/Users/UsersContainer.tsx
@@ -15,6 +15,8 @@ import {
 } from '../../Redux/users-reducer';
 import { Users } from './Users';
 import { Preloader } from '../common/Preloader/Preloader';
+import { usersAPI } from '../../api/Api';
+
 
 type MapStatePropsType = {
   usersPage: InitialStateType;
@@ -52,34 +54,21 @@ export type UsersPropsType = MapStatePropsType & MapDispatchPropsType;
 class UserContainer extends React.Component<UsersContainerPropsType> {
   componentDidMount() {
     this.props.toggleIsFetching(true);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0//users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
-        {
-          withCredentials: true,
-        }
-      )
-      .then((response) => {
-        this.props.toggleIsFetching(false);
-        this.props.setUsers(response.data.items);
-        this.props.setUsersTotalCount(response.data.totalCount);
-      });
+
+    usersAPI.getUsers(this.props.currentPage, this.props.pageSize).then((data) => {
+      this.props.toggleIsFetching(false);
+      this.props.setUsers(data.items);
+      this.props.setUsersTotalCount(data.totalCount);
+    });
   }
 
   onPageChanged = (currentPage: number) => {
     this.props.toggleIsFetching(true);
     this.props.setCurrentPage(currentPage);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0//users?page=${currentPage}&count=${this.props.pageSize}`,
-        {
-          withCredentials: true,
-        }
-      )
-      .then((response) => {
-        this.props.toggleIsFetching(false);
-        this.props.setUsers(response.data.items);
-      });
+    usersAPI.getUsers(this.props.currentPage, this.props.pageSize).then((data) => {
+      this.props.toggleIsFetching(false);
+      this.props.setUsers(data.items);
+    });
   };
 
   render() {
