5e6e84a496aba1c4de51a6d1f72781b65ff7c5d0
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-18 20:40:59 +0300

    Moved requests to the api folder

---
+++ src/api/Api.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import React from 'react'
+
+const instance = axios.create({
+   withCredentials: true,
+   baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+   headers: {
+    'API-KEY': '664b36ce-5bbb-4afc-9ef0-ebea28b98d90',
+  },
+  })
+
+export const usersAPI = {
+  getUsers (currentPage  = 1, pageSize = 10 ) {
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+     .then(response => {
+      return response.data;
+     })
+   }
+}
+
+
+export const followAPI = {
+  setFollow (id: number) {
+    return  instance
+    .delete(
+      `https://social-network.samuraijs.com/api/1.0//follow/${id}`).then(response => {
+        return response.data;
+    })
+  }
+}
+
