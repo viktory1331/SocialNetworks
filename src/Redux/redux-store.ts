ed5cd194edbab13d02054514c9da1a7f2cc47e6c
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-25 16:11:37 +0300

    Applied the applyMiddleware method to the store

--- src/Redux/redux-store.ts
+++ src/Redux/redux-store.ts
@@ -1,10 +1,11 @@
 import React from 'react'
-import { combineReducers, createStore } from 'redux'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
 import { authReducer } from './auth-reducer';
 import { dialogsReducer } from './dialogs-reducer';
 import { profileReducer } from './profile-reducer';
 import { sidebarReducer } from './sidebar-reducer';
 import { usersReducer } from './users-reducer';
+import thunkMiddleware from 'redux-thunk'
 
 let reducers = combineReducers({
    profilePage:profileReducer,
@@ -14,7 +15,7 @@ let reducers = combineReducers({
    auth: authReducer
 });
 
-let store = createStore(reducers)
+let store = createStore(reducers, applyMiddleware(thunkMiddleware))
 
 export type RootStateReduxType = ReturnType <typeof reducers>
 
