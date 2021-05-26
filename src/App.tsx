bfd358574ce1c7ce6c5dae9b375c098bf50ebda5
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-26 13:23:42 +0300

    Added Route to the Login component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import { DialogsContainer } from './Components/Dialogs/DialogsContainer';
 import { Header } from './Components/Header/Header';
 import HeaderContainer from './Components/Header/HeaderContainer';
+import { Login } from './Components/Login/Login';
 import { Music } from './Components/Music/Music';
 import { Navbar } from './Components/Navbar/Navbar';
 import { News } from './Components/News/News';
@@ -22,6 +23,7 @@ function App() {
         <Route path="/dialogs" render={() => <DialogsContainer />} />
         <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
         <Route path="/users" render={() => <UsersContainer />} />
+        <Route path="/login" render={() => <Login />} />
         <Route path="/music" render={() => <Music />} />
         <Route path="/news" render={() => <News />} />
         <Route path="/settings" render={() => <Settings />} />
