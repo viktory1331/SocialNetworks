dc0fae3348a8c7c19ce746970dcf4fa40f9b6175
viktory1331 <viktoriyafedosowa@gmail.com>
2021-06-01 16:45:15 +0300

    dropped the props

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -10,12 +10,14 @@ let pr = {
 
 type ProfilePropsType = {
   profile: UserType | null;
+  status: string;
+  updateStatus: (status: string) => void;
 };
 
 export const Profile = (props: ProfilePropsType) => {
   return (
     <div>
-      <ProfileInfo profile={props.profile} />
+      <ProfileInfo profile={props.profile} status={props.status} updateStatus={props.updateStatus} />
       <MyPostsContainer />
     </div>
   );
