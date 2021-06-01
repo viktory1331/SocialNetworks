15276149b8c8dc972c4f8b9e7fe4350fa2a2e3e8
viktory1331 <viktoriyafedosowa@gmail.com>
2021-06-01 16:42:38 +0300

    Create profileAPI

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -23,7 +23,20 @@ export const usersAPI = {
       })
   },
   getProfile(userId: number) {
+    console.warn('Obsolete method.Please use the profileAPI method')
+    return profileAPI.getProfile(userId)
+  }
+}
+
+export const profileAPI = {
+  getProfile(userId: number) {
     return instance.get(`profile/${userId}`);
+  },
+  getStatus(userId: number) {
+    return instance.get(`profile/status/${userId}`)
+  },
+  updateStatus(status: string) {
+    return instance.put(`profile/status`, { status: status })
   }
 }
 
