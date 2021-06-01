190b7e5c4a4839a6cb43ecd1b744fef6e202868c
viktory1331 <viktoriyafedosowa@gmail.com>
2021-06-01 16:48:09 +0300

    Implemented the input logic

--- src/Components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/Components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import s from './ProfileInfo.module.css';
 
 type ProfileStatusPropsType = {
   status: string;
-  myFullName: string;
+  someFullName: string;
+  updateStatus: (status: string) => void;
 };
 
 export class ProfileStatus extends React.Component<ProfileStatusPropsType> {
   state = {
     editMode: false,
+    status: this.props.status,
   };
 
   activateEditMode = () => {
@@ -16,25 +18,36 @@ export class ProfileStatus extends React.Component<ProfileStatusPropsType> {
       editMode: true,
     });
   };
-
   deactivateEditMode = () => {
     this.setState({
       editMode: false,
+    });
+    this.props.updateStatus(this.state.status);
+  };
+  onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      status: e.currentTarget.value,
     });
   };
 
   render() {
     return (
       <div className={s.profileStatus}>
-        <div className={s.myFullName}>{this.props.myFullName}</div>
+        <div className={s.myFullName}>{this.props.someFullName}</div>
         {!this.state.editMode && (
           <div>
-            <span onClick={this.activateEditMode}>{this.props.status}</span>
+            <span onClick={this.activateEditMode}>
+              {this.props.status || 'No status'}
+            </span>
           </div>
         )}
         {this.state.editMode && (
           <div>
-            <input onBlur={this.deactivateEditMode} value={this.props.status} />
+            <input
+              onChange={this.onStatusChange}
+              onBlur={this.deactivateEditMode}
+              value={this.state.status}
+            />
           </div>
         )}
       </div>
