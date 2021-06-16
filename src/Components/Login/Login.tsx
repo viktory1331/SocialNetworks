5b9df16d1f435ced20a51d6674015fc33f83fb7c
viktory1331 <viktoriyafedosowa@gmail.com>
2021-06-16 13:54:18 +0300

    Added login to the connect and added redirect

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,43 +1,31 @@
 import React from 'react';
-import { Field, InjectedFormProps, reduxForm } from 'redux-form';
-import { required } from '../../utils/validators/Validators';
-import { Input } from '../common/Preloader/FormsControls/FormsControls';
+import { connect } from 'react-redux';
+import { login } from '../../Redux/auth-reducer';
+import { ReduxLoginForm } from './LoginReduxForm';
+import { Redirect } from 'react-router';
+import { RootStateReduxType } from '../../Redux/redux-store';
 
 type FormDataType = {
-  login: string;
+  email: string;
   password: string;
   rememberMe: boolean;
 };
 
 
-const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
-  return (
-    <form onSubmit={props.handleSubmit}>
-      <div>
-        <Field placeholder={'Login'} name={'login'} validate={[required]} component={Input} />
-      </div>
-      <div>
-        <Field placeholder={'Password'} name={'password'} validate={[required]} component={Input} />
-      </div>
-      <div>
-        <Field type={'checkbox'} name={'rememberMe'} component={Input} />
-        remember me
-      </div>
-      <div>
-        <button>Login</button>
-      </div>
-    </form>
-  );
+type PropsType = {
+  login: (email: string, password: string, rememberMe: boolean) => void;
+  isAuth: boolean;
 };
 
-const ReduxLoginForm = reduxForm<FormDataType>({
-  form: 'login',
-})(LoginForm);
 
-export const Login = () => {
+const Login = (props: PropsType) => {
   const onSubmit = (formData: FormDataType) => {
-    console.log(formData);
+    props.login(formData.email, formData.password, formData.rememberMe);
   };
+
+  if (props.isAuth) {
+    return <Redirect to={'/profile'} />;
+  }
 
   return (
     <div>
@@ -46,3 +34,9 @@ export const Login = () => {
     </div>
   );
 };
+
+const mapStateToProps = (state: RootStateReduxType) => ({
+  isAuth: state.auth.isAuth,
+});
+
+export default connect(mapStateToProps, { login })(Login);
